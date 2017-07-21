33d7d5e4e01f10e8450bdaae7e21cbd2da640732
Daniel Raineri <Daniel.Raineri@qa.com>
2017-07-21 14:23:52 +0100

    Friday afternoon push

--- src/main/java/com/qa/cdlibrary/business/CDLibraryServiceDBImpl.java
+++ src/main/java/com/qa/cdlibrary/business/CDLibraryServiceDBImpl.java
@@ -1,5 +1,7 @@
 package com.qa.cdlibrary.business;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 
 import javax.ejb.Stateless;
@@ -31,44 +33,69 @@ public class CDLibraryServiceDBImpl implements CDLibraryService {
 
 	@Override
 	public String createCD(String movie) {
-		CD aCD = util.getObjectForJSON(movie, CD.class);
-		manager.persist(aCD);
-		return "{\"message\": \"cd sucessfully added\"}";
+	    try {
+	        String errorMessage = "";
+            CD aCD = util.getObjectForJSON(movie, CD.class);
+            if (aCD.getArtistName() == null) errorMessage +="artistName, ";
+            if (aCD.getGenre() == null) errorMessage += "genre, ";
+            if (aCD.getAlbumTitle() == null) errorMessage +="albumTitle, ";
+            if (errorMessage.contains("e")) throw new IllegalArgumentException( errorMessage +"is missing");
+            manager.persist(aCD);
+            return "{\"message\": \"cd sucessfully added\"}";
+        }catch (IllegalArgumentException error) {
+	        return  error.getMessage();
+        }
 	}
 
 	@Override
 	public String updateCD(Long id, String aCD) {
 		CD updatedCD = util.getObjectForJSON(aCD, CD.class);
-		CD CDInDB = findCD(id);
-		if (CDInDB != null) {
-			CDInDB = updatedCD;
-			manager.merge(CDInDB);
+		CD cDInDB = findCD(id);
+        if(cDInDB != null){
+            if(updatedCD.getArtistName() != null){ cDInDB.setArtistName(updatedCD.getArtistName());}
+            if(updatedCD.getGenre() != null){ cDInDB.setGenre(updatedCD.getGenre());}
+            if(updatedCD.getAlbumTitle() != null){ cDInDB.setAlbumTitle(updatedCD.getAlbumTitle());}
+            manager.merge(cDInDB);
+			return "{\"message\": \"cd sucessfully updated\"}";
 		}
-		return "{\"message\": \"cd sucessfully updated\"}";
+		return "{\"message\": \"cd with that id doesn't exist\"}";
 	}
 
 	@Override
 	public String deleteCD(Long id) {
-		CD CDInDB = findCD(id);
-		if (CDInDB != null) {
-			manager.remove(CDInDB);
+		CD cDInDB = findCD(id);
+		if (cDInDB != null) {
+			manager.remove(cDInDB);
+			return "{\"message\": \"cd sucessfully deleted\"}";
 		}
-		return "{\"message\": \"cd sucessfully deleted\"}";
+		else{
+		    return "{\"message\": \"cd with that id doesn't exist\"}";
+        }
+
 	}
 
 	@Override
 	public String deleteAllCD() {
         Query query = manager.createQuery("DELETE FROM CD");
         query.executeUpdate();
-        return "{\"message\": \"all cd sucessfully deleted\"}";
+        return "{\"message\": \"all cd successfully deleted\"}";
 	}
 
-	@Override
+/*	@Override
     public String findCDByID(Long id){
         String findCDByIDQuery = "Select c FROM CD c WHERE id = "+id;
         Query query = manager.createQuery(findCDByIDQuery);
         Collection<CD> results = (Collection<CD>) query.getResultList();
         return util.getJSONForObject(results);
+    }
+*/
+	@Override
+    public String findCDByID(Long id){
+        CD cDInDB = findCD(id);
+        if (cDInDB != null) {
+            return util.getJSONForObject(cDInDB);
+        }
+        return "{\"message\": \"cd with that id doesn't exist\"}";
     }
 
 	private CD findCD(Long id) {
